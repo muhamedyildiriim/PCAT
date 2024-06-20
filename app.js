b4464b77b6c7db91e02d4be60d1747de46cfbf55
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-20 11:59:08 +0300

    SPhotos folder created, photos added to single photo page, photo previews created in index.html.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -15,10 +18,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // ROUTES
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', { photos });
 });
 
@@ -29,9 +33,9 @@ app.get('/index.html', async (req, res) => {
 
 app.get('/photos/:id', async (req, res) => {
   // console.log(req.params.id)
-  const photo = await Photo.findById(req.params.id)
+  const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
+    photo,
   });
 });
 
@@ -44,9 +48,26 @@ app.get('/about.html', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  console.log(req.body);
-  res.redirect('/');
+  console.log(req.files.image);
+  // await Photo.create(req.body);
+  // res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
