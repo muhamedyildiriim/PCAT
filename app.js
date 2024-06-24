cfa15a5c0acbb8368816fe000ac800631891d732
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-24 14:23:40 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const pageController = require('./controllers/pageControllers')
 const app = express();
 
 // CONNECT DB
-mongoose.connect('mongodb://localhost/pcat-test-db', {});
+mongoose.connect('mongodb+srv://muhamedd:pM56Gn7GxtJgfDP@cluster0.bxpyoz4.mongodb.net/pcat-db?retryWrites=true&w=majority&appName=Cluster0', {})
+.then(() => {
+  console.log('DB Connected')
+}).catch(() => {
+  console.log(err)
+});
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -38,7 +43,7 @@ app.get('/about.html', pageController.getAboutPage);
 
 app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
